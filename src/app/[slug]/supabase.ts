9efca10ba03e9e37337eb5f4390be6c4a14fd2a5
Manohar Pedapenki <manupedapenki@gmail.com>
2026-05-12 15:02:01 +0530

    Security: Moved hardcoded secrets to env variables

--- src/app/[slug]/supabase.ts
+++ src/app/[slug]/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// ✅ THIS IS THE SECURE PRO WAY
+// ✅ THE SECURE "PRO" WAY
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
